980bcfa889a880c3d1f374c60389a4903918ace6
DRIKA <adrielle.letras@gmail.com>
2022-07-18 18:37:17 -0300

    Projeto encurtador de URL

---
+++ encurtador-de-url/src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+    API_URL: "http://localhost:5000",
+    
+    MONGO_CONNECTION:
+		'mongodb+srv://drikUser:FlautaOdul@d3@encurtador-url.brsfc.mongodb.net/?retryWrites=true&w=majority',
+}
